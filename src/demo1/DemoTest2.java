db86f394333b859464c7ac1bc1a81f8226da0815
ericw <ericw@UncleRicco.san.rr.com>
2025-02-04 20:19:23 -0800

    first commit 02/04/2025

--- src/demo1/DemoTest2.java
+++ src/demo1/DemoTest2.java
@@ -3,5 +3,14 @@ package demo1;
 public class DemoTest2 {
 public static void main(String[] args) {
 	System.out.println("Demo Test 2!");
+	
+	int a = 30;
+	int b = 56;
+	
+	int result = a + b;
+	
+	
+	System.out.println(result);
+	
 }
 }
